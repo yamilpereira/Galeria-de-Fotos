73439d5f55cb5ba25a649458f3420bde5052b600
YAMIL <losmasbuscadosjson1995@gmail.com>
2017-11-27 13:05:24 -0400

    Terminado LOG de usuario

--- public/admin/logfile.php
+++ public/admin/logfile.php
@@ -1,15 +1,21 @@
 <?php require_once("../../includes/initialize.php");?>
 <?php if(!$sesion->estalogueado()){  header('Location: login.php');}?>
-<?php incluir_plantillas("admin_header.php")?>
 <?php
+  $archivo_txt="/var/www/html/Php/intermedio/Semana 3/GaleriaMejorado/logs/log.txt";
+  /*   is_readable — Indica si un fichero existe y es legible
+  Devuelve TRUE si el fichero o directorio especificado por filename existe y es legible, FALSE si no. */
+  /*
+  trim — Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el final de la cadena
+  */
+  if(isset($_GET["limpiar"]) && $_GET["limpiar"]=="1")
+  {
+     file_put_contents($archivo_txt,"");
+     grabar_acciones("limpiar"," El usuario ". $sesion->id." has limpiado el archivo log");
+     header('Location: logfile.php');
+  }
 
-$archivo_txt="/var/www/html/Php/intermedio/Semana 3/GaleriaMejorado/logs/log.txt";
-/*   is_readable — Indica si un fichero existe y es legible
-Devuelve TRUE si el fichero o directorio especificado por filename existe y es legible, FALSE si no. */
-/*
-trim — Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el final de la cadena
-*/
 ?>
+<?php incluir_plantillas("admin_header.php")?>
     <table>
       <thead>
         <tr>
@@ -17,7 +23,6 @@ trim — Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el f
         </tr>
       </thead>
       <tbody>
-
           <?php
             if(file_exists($archivo_txt) && is_readable($archivo_txt) && $archivo=fopen($archivo_txt,"r"))
             {
@@ -30,6 +35,9 @@ trim — Elimina espacio en blanco (u otro tipo de caracteres) del inicio y el f
               fclose($archivo);
             }
           ?>
+          <tr>
+            <th colspan="6" style="text-align:center;color: #303030"><a href="logfile.php?limpiar=1" style="color: #303030">Eliminar el archivo</a></th>
+          </tr>
       </tbody>
     </table>
 <?php incluir_plantillas("footer.php")?>
